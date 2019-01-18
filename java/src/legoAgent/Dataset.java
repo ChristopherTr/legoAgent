7407fdcb2e117024ccdadadebe3032017f22b8c7
daniel <daniel@goldenghost.de>
2019-01-18 16:56:01 +0100

    Implemented dataset and data, first drafts

--- java/src/legoAgent/Dataset.java
+++ java/src/legoAgent/Dataset.java
@@ -1,41 +1,109 @@
 package legoAgent;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+
 public class Dataset implements IDataset {
 
+	/**
+	 * the path and filename in the filesystem of the serialized data.
+	 */
+	static private String fileLocation = "location.data";
+	private ArrayList<IDatum> dataset;
+	private IDatum[] sVMPoints;
+	
+	public Dataset() {
+		this.load();
+	}
+	
+	/**
+	 * stores the current status of the dataset to the disk. 
+	 * Storage-path is specified by static variable fileLocation. 
+	 * TODO: storage of SVMPoints in separate file
+	 */
 	@Override
 	public void store() {
-		// TODO Auto-generated method stub
-
+		try {
+			FileOutputStream fos = new FileOutputStream(fileLocation);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(dataset);
+			oos.close();
+			fos.close();
+		} catch (IOException e) {
+			Logger.log(e.getMessage());
+		} finally {
+			Logger.log("Objektspeicherung beendet");
+		}
 	}
 
+	/**
+	 * loads the data that are currently stored in the filesystem. 
+	 * Path is specified by static variable fileLocation.
+	 * TODO: Load of SVMPoints from separate file 
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void load() {
-		// TODO Auto-generated method stub
-
+		try {
+			FileInputStream fis = new FileInputStream(fileLocation);
+		    ObjectInputStream ois = new ObjectInputStream(fis);
+		    this.dataset = (ArrayList<IDatum>) ois.readObject();
+		    ois.close();
+		    fis.close();
+		} catch (IOException | ClassNotFoundException e) {
+			Logger.log(e.getMessage());
+			this.dataset = new ArrayList<IDatum>();
+		} finally {
+			Logger.log("Objekte geladen");
+		}
+	}
+	
+	/**
+	 * removes the currently existing dataset, does not store this changes to disk. 
+	 */
+	@Override
+	public void clearAll() {
+		this.dataset = new ArrayList<IDatum>();
+		this.removeStoredSVMPoints();
 	}
 
+	/**
+	 * Adds a new data to the dataset in RAM, does not store this changes to disk. 
+	 */
 	@Override
 	public void addNewData(IDatum data) {
-		// TODO Auto-generated method stub
-
+		this.dataset.add(data);
 	}
 
+	/**
+	 * returns the current dataset from RAM. 
+	 */
 	@Override
-	public IDatum[] getAllData() {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<IDatum> getAllData() {
+		return this.dataset;
 	}
 
+	/**
+	 * returns the current SVM-Points to create the border, 
+	 * or null, if no data is stored
+	 */
 	@Override
 	public IDatum[] getSVMPoints() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.sVMPoints;
 	}
 
+	/**
+	 * removes the currently stored SVM-Points. 
+	 * Used e.g. in case of addition of a new datapoint to the dataset. 
+	 */
 	@Override
 	public void removeStoredSVMPoints() {
-		// TODO Auto-generated method stub
-
+		this.sVMPoints = null;
 	}
-
 }
--- java/src/legoAgent/Datum.java
+++ java/src/legoAgent/Datum.java
@@ -1,23 +1,57 @@
 package legoAgent;
 
-public class Datum implements IDatum {
+import java.io.Serializable;
 
+public class Datum implements IDatum, Serializable {
+
+	
+	/**
+	 * ID for serializing of objects
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * periemeter and area of this data
+	 */
+	private int perimeter, area;
+	/**
+	 * Type of figure
+	 */
+	private Figure figure;
+	
+	public Datum(int perimeter, int area) {
+		this.area = area;
+		this.perimeter = perimeter;
+		this.figure = Figure.UNKNOWN;
+	}
+	
+	public Datum(int perimeter, int area, Figure figure) {
+		this.area = area;
+		this.perimeter = perimeter;
+		this.figure = figure;
+	}
+	
+	/**
+	 * Returns the perimeter of this data. 
+	 */
 	@Override
 	public int getPerimeter() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.perimeter;
 	}
 
+	/**
+	 * returns the area of this data.
+	 */
 	@Override
 	public int getArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.area;
 	}
 
+	/**
+	 * returns the type of figure of this data, if set. 
+	 * By constructor, if no figure is specified, Figure.UNKNOWN is set as value
+	 */
 	@Override
 	public Figure getFigure() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.figure;
 	}
-
 }
