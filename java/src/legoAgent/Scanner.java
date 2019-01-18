054514dbb39aa7dea384a2d001f8be429c216274
daniel <daniel@goldenghost.de>
2019-01-18 16:55:33 +0100

    implemented scanner-basics

--- java/src/legoAgent/Scanner.java
+++ java/src/legoAgent/Scanner.java
@@ -2,10 +2,17 @@ package legoAgent;
 
 public class Scanner implements IScanner {
 
+	public final static int resolution = 512;
+	
 	@Override
-	public int[][] bildLesen() {
-		// TODO Auto-generated method stub
+	public int[][] readImage() {
 		return null;
+	}
+
+	@Override
+	public int[] computeTrait(int[][] image) {
+		// TODO Auto-generated method stub
+		return null; // BITTE UMFANG AN STELLE 0, FLÄCHE AN STELLE 1
 	}
 
 }
