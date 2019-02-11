3e21bcb62a6a2bd0567b3de8f2afdb2f114e2897
Christopher T <trdrchristopher@gmail.com>
2019-02-11 10:37:22 +0100

    removed unecessary libs

--- leImRo/src/leImRo/SVM.java
+++ leImRo/src/leImRo/SVM.java
@@ -1,6 +1,5 @@
 package leImRo;
 import java.util.ArrayList;
-import org.ejml.simple.SimpleMatrix;
 
 public class SVM implements ISVM {
 
@@ -69,13 +68,9 @@ public class SVM implements ISVM {
 				svmPoints[2] = circleArray[0];
 				this.dataSet.setsVMPoints(svmPoints);
 			}
-		} else { // Difficult case: more than three points are avail. 
-			examineSupportVectors(listRectangle, listCircle, classifierRectangle, classifierCircle);
-			examineSupportVectors(listCircle, listRectangle, classifierCircle, classifierRectangle);
-			
-			// Viel Spaß Benny :P
-		}
-		
+		} else { 
+			throw new IllegalArgumentException("Too much trainingsdata to compute support-vectors: " + size + ", requered: 3");
+		}		
 		return null;
 	}
 
@@ -137,50 +132,4 @@ public class SVM implements ISVM {
 			}
 		}
 	}
-	
-	//ToDo: Return the best "separator"
-	//Aufbau Matrix
-	//  		Node 1	Node 2	Node 3	Classification
-	//Perimeter	a00     a01		a02		1/-1
-	//Area		a10		a11		a12		1/-1
-	//Default	1		1		1		1/-1
-	private void examineSupportVectors(ArrayList<IDataPoint> hostList, ArrayList<IDataPoint> slaveList, int firstClassifier, int secondClassifier) {
-		SimpleMatrix nodeMatrix = new SimpleMatrix(3, 3);
-		
-		//Klassifikationsvektor: Aufbau statisch, da innerhalb der Methode immer beispielsweise 1* Kreis und 2*Rechteck genutzt wird
-		SimpleMatrix classificationVector = new SimpleMatrix(3,1);
-		classificationVector.setRow(0, 0,firstClassifier);
-		classificationVector.setRow(1, 0,secondClassifier);
-		classificationVector.setRow(2, 0,secondClassifier);
-		
-		for(IDataPoint hostDatum: hostList) {
-			for(IDataPoint firstSlaveDatum: slaveList) {
-				for(IDataPoint secondSlaveDatum: slaveList) {
-					nodeMatrix.set(0, 0, (hostDatum.getPerimeter()*2+hostDatum.getArea()*2+1));    //a00
-					nodeMatrix.set(0, 1, (hostDatum.getPerimeter()*firstSlaveDatum.getPerimeter()+hostDatum.getArea()*firstSlaveDatum.getArea()+1));    //a01
-					nodeMatrix.set(0, 2, (hostDatum.getPerimeter()*secondSlaveDatum.getPerimeter()+hostDatum.getArea()*secondSlaveDatum.getArea()+1));    //a02
-				    
-					nodeMatrix.set(1, 0, (firstSlaveDatum.getPerimeter()*hostDatum.getPerimeter()+firstSlaveDatum.getArea()*hostDatum.getArea()+1));    		//a10
-					nodeMatrix.set(1, 1, (firstSlaveDatum.getPerimeter()*2+firstSlaveDatum.getArea()*2+1));													//a11	
-					nodeMatrix.set(1, 2, (firstSlaveDatum.getPerimeter()*secondSlaveDatum.getPerimeter()+firstSlaveDatum.getArea()*secondSlaveDatum.getArea()+1));    //a12
-				
-					nodeMatrix.set(2, 0, (secondSlaveDatum.getPerimeter()*hostDatum.getPerimeter()+secondSlaveDatum.getArea()*hostDatum.getArea()+1));    		//a20
-					nodeMatrix.set(2, 1, (secondSlaveDatum.getPerimeter()*firstSlaveDatum.getPerimeter()+secondSlaveDatum.getArea()*firstSlaveDatum.getArea()+1));//a21													//a11	
-					nodeMatrix.set(2, 2, (secondSlaveDatum.getPerimeter()*2+secondSlaveDatum.getArea()*2+1));    													//a22	
-					
-					//Berechnung der Alphawerte
-					SimpleMatrix alphaValues = nodeMatrix.solve(classificationVector);
-					
-					
-					
-					
-					// Aufruf, damit die Klasse ohne Fehler ist: 
-					alphaValues.bits();
-					//Berechnung des w- Vektors zur Beurteilung der Hyperebene
-					//tbd...
-				}
-			}
-		}
-	}
-
 }
