ac518712bd95b033bba322f210e024247eed3ebe
wmdieter <36644408+wmdieter@users.noreply.github.com>
2019-02-13 09:54:15 +0100

    adding comments

--- leImRo/src/leImRo/Main.java
+++ leImRo/src/leImRo/Main.java
@@ -1,19 +1,23 @@
 /**
- * Demo Project to test the hardware of the roboter
+ * Startpunkt fuer die Roboter-Anwendung
  */
 package leImRo;
 
 /**
- * @author trdrc
+ * @author Christopher Traub
+ * @author Jens Eisele
+ * @author Daniel Koelbel
+ * @author Benjamin Baechle
+ * @author Wolf-Michael Dieter
  *
  */
 public class Main {
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
+		//Die Klasse Recognition kappselt alle benötigten Funktionen
 		Recognition recognition = new Recognition();
 		
+		// Das Menue wird dauerhaft angezeigt und kann über die Referenz auf die Recognition Instanz alle Programmteile starten.
+		// Nach Abschluss eines Programmteils wird erneut das Menue angezeigt.
 		while(true) {
 			GUI.processMenu(recognition);
 		}
