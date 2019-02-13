acd72e526c12ff4cae85ab7da32ed9b0f3c73c18
daniel <daniel@goldenghost.de>
2019-02-13 11:54:25 +0100

    added logging to file

--- leImRo/src/leImRo/Logger.java
+++ leImRo/src/leImRo/Logger.java
@@ -1,13 +1,31 @@
 package leImRo;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class Logger {
 	
 	public static final String fileLocation = "logger.txt";
+	private static FileWriter fw; 
+	private static BufferedWriter bw;
+	static {
+		try {
+			fw = new FileWriter(fileLocation);
+			bw = new BufferedWriter(fw);
+		} catch (Exception e) {
+			// Silently ignore Exceptions ;)
+		}
+	}
 	
 	public static void log(String str) {
 		Long time = System.currentTimeMillis();
 		String logline = String.format("%d - " + str, time);
 		System.out.println(logline);
+		try {
+			bw.write(logline);
+		} catch (Exception e) {
+			// Silently ignore Exceptions ;)
+		}
 	}
 	
 }
